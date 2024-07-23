5b3918326285a1623c04fa0e54beb00d1ea060f4
prasadhonrao <honrao.prasad@gmail.com>
2024-07-23 12:21:52 +0100

    Implement search by name or description functionality

--- src/data/products.js
+++ src/data/products.js
@@ -2,21 +2,25 @@ const products = [
   {
     id: 1,
     name: 'Product 1',
+    description: 'This is a description for product 1',
     price: 100,
   },
   {
     id: 2,
     name: 'Product 2',
+    description: 'This is a description for product 2',
     price: 200,
   },
   {
     id: 3,
     name: 'Product 3',
+    description: 'This is a description for product 3',
     price: 300,
   },
   {
     id: 4,
     name: 'Product 4',
+    description: 'This is a description for product 4',
     price: 400,
   },
 ];
--- src/index.js
+++ src/index.js
@@ -9,18 +9,45 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express();
 
+// desc   Get the root API
+// route  GET /
+// access Public
 app.get('/', (req, res) => {
   return res.status(200).send('Welcome to Node Product Microservice!');
 });
 
+// desc   Get API version
+// route  GET /api/version
+// access Public
 app.get('/version', (req, res) => {
   return res.status(200).send('1.0.0');
 });
 
+// desc   Get all products
+// route  GET /api/products
+// access Public
 app.get('/api/products', (req, res) => {
   return res.status(200).send(products);
 });
 
+// desc   Search for products by name or by description using query parameters
+// route  GET /api/products/search?keyword=product
+// access Public
+app.get('/api/products/search', (req, res) => {
+  const { keyword } = req.query;
+  if (!keyword) {
+    return res.status(400).send({ message: 'Bad Request. Missing keyword parameter' });
+  }
+  const results = products.filter((p) => p.name.includes(keyword) || p.description.includes(keyword));
+  if (results.length === 0) {
+    return res.status(200).send({ message: 'No products found' });
+  }
+  return res.status(200).send(results);
+});
+
+// desc   Get a product by ID
+// route  GET /api/products/:id
+// access Public
 app.get('/api/products/:id', (req, res) => {
   // Check if the id is a number
   if (isNaN(req.params.id)) {
